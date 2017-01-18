6e74d3715704e16d82584ab5d7628d6572ea8132
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-17 22:41:18 -0800

    updated static site

--- assets/try-online/marko$4.0.0-rc.3/runtime/index.js
+++
@@ -1,7 +0,0 @@
-$_mod.def("/marko$4.0.0-rc.3/runtime/index", function(require, exports, module, __filename, __dirname) { 'use strict';
-{}/*require('./env-init');*/ // no-op in the browser, but enables extra features on the server
-
-exports.createOut = require('/marko$4.0.0-rc.3/runtime/createOut'/*'./createOut'*/);
-exports.load = require('/marko$4.0.0-rc.3/runtime/loader/index-browser-dynamic'/*'./loader'*/);
-exports.events = require('/marko$4.0.0-rc.3/runtime/events'/*'./events'*/);
-});
