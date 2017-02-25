c8a290b2de1f1c20e7e30f8804d3297aeecc76ce
Michael Rawlings <ml.rawlings@gmail.com>
2017-02-24 19:40:32 -0800

    updated static site

--- assets/docs/warp10$1.3.3/parse.js
+++
@@ -1,2 +0,0 @@
-$_mod.def("/warp10$1.3.3/parse", function(require, exports, module, __filename, __dirname) { module.exports = require('/warp10$1.3.3/src/parse'/*'./src/parse'*/);
-});
--- assets/index/marko$4.0.0-rc.12/widgets/loadWidget-dynamic.js
+++
@@ -1,9 +0,0 @@
-$_mod.def("/marko$4.0.0-rc.12/widgets/loadWidget-dynamic", function(require, exports, module, __filename, __dirname) { 'use strict';
-
-module.exports = function load(typeName) {
-    // We make the assumption that the widget type name is a path to a
-    // fully resolved module path and that the module exists
-    // as a CommonJS module
-    return require(typeName);
-};
-});
